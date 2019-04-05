12a591d89b0f543153482f8e2671fdcc1e6aab9b
tobiassandoelund <36983846+tobiassandoelund@users.noreply.github.com>
2019-04-05 11:46:39 +0200

    Basehandler

---
+++ ConsoleApp2/BaseHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp2
+{
+    public abstract class BaseHandler : IHandler
+    {
+        private IHandler _nextHandler { get; set; }
+
+        public void SetNext(IHandler handler)
+        {
+            _nextHandler = handler;
+        }
+
+        public abstract void Handle(IRequest request);
+    }
+}
