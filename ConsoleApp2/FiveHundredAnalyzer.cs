5292e0d45317780a9b1a158ff76e8b24eaac1391
ALE <holmern251@gmail.com>
2019-04-05 13:58:43 +0200

    200+500

---
+++ ConsoleApp2/FiveHundredAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2
+{
+    public class FiveHundredAnalyzer : BaseHandler
+    {
+        public FiveHundredAnalyzer(IHandler handler) : base(handler)
+        {
+
+        }
+
+        public FiveHundredAnalyzer() : base()
+        {
+
+        }
+
+
+        protected override void SpecificHandle(IRequest request)
+        {
+            request.Amount -= 500;
+            request.Bills.Add(new Bill("500 kr."));
+        }
+
+        protected override bool CanHandle(IRequest request)
+        {
+            return request.Amount >= 500;
+        }
+    }
+}
---
+++ ConsoleApp2/TwoHundredAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp2
+{
+    public class TwoHundredAnalyzer : BaseHandler
+    {
+        public TwoHundredAnalyzer(IHandler handler) : base(handler)
+        {
+            
+        }
+
+        public TwoHundredAnalyzer() : base()
+        {
+            
+        }
+
+
+        protected override void SpecificHandle(IRequest request)
+        {
+            request.Amount -= 200;
+            request.Bills.Add(new Bill("200 kr."));
+        }
+
+        protected override bool CanHandle(IRequest request)
+        {
+            return request.Amount >= 200;
+        }
+    }
+}
